e59450a4bf1e2e94e1e68c6e9326ec836ad90f05
happy <gillharpreetsingh211@gmail.com>
2025-03-03 21:14:26 +0530

    Using unwrap() and expect()

--- Error_Handling/src/main.rs
+++ Error_Handling/src/main.rs
@@ -2,21 +2,26 @@ use std::fs::File;
 use std::io::ErrorKind;
 
 fn main() {
-    let file_result = File::open("hello.txt");
+    // let file_result = File::open("hello.txt");
 
-    let file = match file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => {
-                println!("File not found, creating a new one...");
-                File::create("hello.txt").unwrap()
-                // File::create("hello.txt").unwrap()
-            }
-            other_error => {
-                panic!("Problem opening file: {:?}", other_error);
-            }
-        },
-    };
+    // let file = match file_result {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => {
+    //             println!("File not found, creating a new one...");
+    //             File::create("hello.txt").unwrap()
+    //             // File::create("hello.txt").unwrap()
+    //         }
+    //         other_error => {
+    //             panic!("Problem opening file: {:?}", other_error);
+    //         }
+    //     },
+    // };
 
-    panic!("Something went wrong!");
+    // panic!("Something went wrong!");
+
+    // Using unwrap() and expect()
+
+    let file = File::open("hello.txt").unwrap();
+    let file = File::open("hello.txt").expect("Failed to open file");
 }
