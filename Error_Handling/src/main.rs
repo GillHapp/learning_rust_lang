0013cb900964cb55d075f1427ed0bb8e50d89769
happy <gillharpreetsingh211@gmail.com>
2025-03-03 21:08:02 +0530

    Recoverable Errors (Result<T, E>)

---
+++ Error_Handling/src/main.rs
@@ -0,0 +1,20 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let file_result = File::open("hello.txt");
+
+    let file = match file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => {
+                println!("File not found, creating a new one...");
+                File::create("hello.txt").unwrap()
+                // File::create("hello.txt").unwrap()
+            }
+            other_error => {
+                panic!("Problem opening file: {:?}", other_error);
+            }
+        },
+    };
+}
