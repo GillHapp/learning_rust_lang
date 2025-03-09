924ce6780af6011d64d59e0a035071dd46cb8c8e
happy <gillharpreetsingh211@gmail.com>
2025-03-09 22:23:30 +0530

    Closures as Function Parameters

--- closers/src/main.rs
+++ closers/src/main.rs
@@ -1,9 +1,8 @@
+fn apply<F: Fn(i32)>(f: F, x: i32) {
+    f(x);
+}
+
 fn main() {
-    let mut list = vec![1, 2, 3];
-    println!("Before defining closure: {list:?}");
-
-    let mut borrows_mutably = || list.push(7);
-
-    borrows_mutably();
-    println!("After calling closure: {list:?}");
+    let print_num = |num| println!("Number: {num}");
+    apply(print_num, 10);
 }
