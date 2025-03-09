68109cc3628244251ba9b0f369f694445f1f89f1
happy <gillharpreetsingh211@gmail.com>
2025-03-09 22:29:10 +0530

    Closures Returning Closures

--- closers/src/main.rs
+++ closers/src/main.rs
@@ -1,8 +1,11 @@
-fn apply<F: Fn(i32)>(f: F, x: i32) {
-    f(x);
+pub mod modname {
+    pub fn make_multiplier(multiplier: i32) -> impl Fn(i32) -> i32 {
+        move |x| x * multiplier
+    }
 }
 
+use crate::modname::make_multiplier;
 fn main() {
-    let print_num = |num| println!("Number: {num}");
-    apply(print_num, 10);
+    let double = make_multiplier(2);
+    println!("{}", double(5)); // Output: 10
 }
